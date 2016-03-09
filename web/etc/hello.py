2c10a9c140f376cb4783c59d699af5f341a7a1a0
AleksStek <aleksstek@yandex.ru>
2016-03-09 09:57:04 +0300

    gunicorn trouble

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,6 +1,4 @@
-bind = '127.0.0.1:8080'
+bind = '0.0.0.0:8080'
 daemon = True
 workers = 2
 timeout = 60
-accesslog = '/home/box/web/gunicorn.log'
-pidfile = '/home/box/web/gunicorn.pid'
--- web/hello.py
+++ web/hello.py
@@ -1,8 +1,8 @@
-def wsgi_application(environ, start_response):
+def app (environ, start_response):
 	status = '200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
 	]
-	body = 'hello world!'
+	body = environ['QUERY_STRING'].replace('&','\r\n')
 	start_response(status, headers)
 	return [body]
