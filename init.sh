32de6b96dc6a003a4aebdee044db9bf106e66726
AleksStek <aleksstek@yandex.ru>
2016-03-09 08:42:17 +0300

    Second lab on stepic

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
---
+++ web/etc/hello.py
@@ -0,0 +1,6 @@
+bind = '127.0.0.1:8080'
+daemon = True
+workers = 2
+timeout = 60
+accesslog = '/home/box/web/gunicorn.log'
+pidfile = '/home/box/web/gunicorn.pid'
