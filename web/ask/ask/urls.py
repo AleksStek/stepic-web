8a1ecce768373fa50c66db02550466977bd5323a
AleksStek <aleksstek@yandex.ru>
2016-03-14 10:19:36 +0300

    Add pagination and page with questions and answers

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from qa.views import test, basetemp
+from qa.views import test, basetemp, questpage
 
 urlpatterns = [
-	url(r'^$', test),
+	url(r'^$', basetemp, {'url':'/?page=','order':'-added_ad'}),
 	url(r'^login/.*$', test),
 	url(r'^signup/.*$', test),
-	url(r'^question/.*$', test),
+	url(r'^question/(?P<slug>\d+)/$', questpage),
 	url(r'^ask/.*$', test),
-	url(r'^popular/.*$', test),
+	url(r'^popular/.*$', basetemp, {'url':'/popular/?page=','order':'-likes'}),
 	url(r'^new/.*$', test),
-	url(r'^base/.*$', basetemp),
 ]
--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from models import Question, Answer
+from functions import pagepag
+
 def test(request, *args, **kwargs):
 	return HttpResponse('OK')
 
-def basetemp(request, *args):
-	return render(request, 'index.html',)
+def basetemp(request, url, order='-added_ad'):
+	questions = Question.objects.all()
+	questions = questions.order_by(order)
+	[paginator,page] = pagepag(request, questions, url)
+	return render(request, 'index.html', {
+		'questions': page.object_list,
+		'paginator': paginator,
+		'page': page,
+	},)
+
+def questpage(request, slug):
+	question = get_object_or_404(Question, pk=slug)
+	return render(request, 'quest.html',{
+		'question': question,
+		'answers': Answer.objects.filter(question=slug).order_by('-added_ad')[:]
+	},)
